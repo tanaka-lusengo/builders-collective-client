3a817b3caa0d3e3cd4c105b80e76c0535c9ae2f0
Tanaka Lusengo <79418686+tanaka-lusengo@users.noreply.github.com>
2022-03-24 11:33:12 +0000

    install required packages, set-up initial routes and folders

---
+++ src/pages/PostJobs/PostJobs.js
@@ -0,0 +1,6 @@
+import React from "react";
+import "./PostJobs.scss";
+
+export default function PostJobs() {
+  return <div>PostJobs</div>;
+}
---
+++ src/pages/PostJobs/PostJobsDetail/PostJobsDetail.js
@@ -0,0 +1,6 @@
+import React from "react";
+import "./PostJobsDetail.scss";
+
+export default function PostJobsDetail() {
+  return <div>PostJobsDetail</div>;
+}
