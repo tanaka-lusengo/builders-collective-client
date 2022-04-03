07def62814f2c112b909d0f928568b8ad7f994b2
Tanaka Lusengo <79418686+tanaka-lusengo@users.noreply.github.com>
2022-04-03 03:20:51 +0100

    polished messages feature, to attempt user profile next

--- src/components/MyNetworkChat/MyNetworkChat.js
+++ src/components/MyNetworkChat/MyNetworkChat.js
@@ -1,35 +1,42 @@
 import React from "react";
 import { useState } from "react";
 import "./MyNetworkChat.scss";
-import { Timestamp } from "../../utilities/helper";
+import {
+  TimestampChat,
+  handlePageScrollBottom,
+  handlePageScroll,
+} from "../../utilities/helper";
 import { useEffect } from "react";
 import { v4 as uuid4 } from "uuid";
+import SendIcon from "@mui/icons-material/Send";
+import CloseIcon from "@mui/icons-material/Close";
+import GroupIcon from "@mui/icons-material/Group";
 import ScrollToBottom from "react-scroll-to-bottom";
+import io from "socket.io-client";
 
-export default function MyNetworkChat({ socket, username, room }) {
+const socket = io.connect("http://localhost:3001");
+
+socket.on("message", (message) => {
+  console.log(message);
+});
+
+export default function MyNetworkChat({ socket, username, room, setShowChat }) {
   const [currentMessage, setCurrentMessage] = useState("");
   const [messageFeed, setMessageFeed] = useState([]);
 
-  // function to get events for state
-  //   const handleCurrentMessage = (e) => {
-  //     e.preventDefault();
-  //     let form = e.target;
-  //     const messageVal = form.message.value;
-  //     setCurrentMessage(messageVal);
-  //   };
-
   //send message function
   const sendMessage = async () => {
-    if (currentMessage !== "") {
+    if (currentMessage) {
       const messageData = {
         room: room,
         author: username,
         content: currentMessage,
-        time: Timestamp(Date.now()),
+        time: TimestampChat(Date.now()),
       };
       try {
         await socket.emit("send_message", messageData);
         setMessageFeed((prevMessages) => [...prevMessages, messageData]);
+        setCurrentMessage("");
       } catch (err) {
         console.log("sendMessage socket error -->", err);
       }
@@ -44,67 +51,79 @@ export default function MyNetworkChat({ socket, username, room }) {
   }, [socket]);
 
   return (
-    <div className="chat-container">
-      <header className="chat-header">
-        <h1>
-          <i className="fas fa-smile"></i> ChatCord
-        </h1>
-        <a href="index.html" className="btn">
-          Leave Room
-        </a>
-      </header>
-      <main className="chat-main">
-        {/* <div className="chat-sidebar">
-          <h3>
-            <i className="fas fa-comments"></i> Room Name:
-          </h3>
-          <h2 id="room-name">JavaScript</h2>
-          <h3>
-            <i className="fas fa-users"></i> Users
-          </h3>
-          <ul id="users">
-            <li>Brad</li>
-            <li>John</li>
-            <li>Mary</li>
-            <li>Paul</li>
-            <li>Mike</li>
-          </ul>
-        </div> */}
-        <div className="chat-messages">
-          <div className="message">
-            {messageFeed.map((message) => {
-              return (
-                <>
-                  <p className="meta">
-                    {message.author} <span>{message.time}</span>
-                  </p>
-                  <p className="text" key={uuid4()}>
-                    {message.content}
-                  </p>
-                </>
-              );
-            })}
+    <section className="network-chat">
+      <div className="network-chat__layer">
+        <div className="network-chat__layer-content">
+          <header className="network-chat__header">
+            <h1 className="network-chat__title">Chat Stream</h1>
+            <div className="network-chat__close-container">
+              <CloseIcon />
+              <p
+                className="network-chat__leave"
+                onClick={() => setShowChat(false)}
+              >
+                Leave Room
+              </p>
+            </div>
+          </header>
+          <main className="network-chat__main">
+            <div className="network-chat__sidebar">
+              <h3 className="network-chat__comments"></h3>
+              <h2 className="network-chat__room-name">Builders' Collective</h2>
+              <div className="network-chat__room-title-container">
+                <GroupIcon />
+
+                <h3 className="network-chat__users-title">Users</h3>
+              </div>
+              <ul className="network-chat__users-list">
+                <li className="network-chat__users-list-item">Brad</li>
+              </ul>
+            </div>
+            <div className="network-chat__chat-messages-container">
+              <ScrollToBottom className="network-chat__messages-container-scroll">
+                {messageFeed.map((message) => {
+                  return (
+                    <>
+                      <div
+                        className={
+                          "network-chat__messages network-chat__messages" +
+                          (username === message.author ? "" : "--other")
+                        }
+                      >
+                        <h3 className="network-chat__author">
+                          {message.author}:
+                        </h3>
+                        <p className="network-chat__content">
+                          {message.content}
+                        </p>
+                      </div>
+                      <p className="network-chat__time">{message.time}</p>
+                    </>
+                  );
+                })}
+              </ScrollToBottom>
+            </div>
+          </main>
+          <div className="network-chat__form-container">
+            <input
+              className="network-chat__form-input"
+              type="text"
+              value={currentMessage}
+              name="message"
+              placeholder="Enter Message"
+              required
+              autoComplete="off"
+              onChange={(e) => setCurrentMessage(e.target.value)}
+              onKeyDown={(e) => {
+                e.key === "Enter" && sendMessage();
+              }}
+            />
+            <span className="network-chat__form-send" onClick={sendMessage}>
+              <SendIcon />
+            </span>
           </div>
         </div>
-      </main>
-      <div className="chat-form-container">
-        {/* <form id="chat-form" > */}
-        <input
-          type="text"
-          name="message"
-          placeholder="Enter Message"
-          required
-          autoComplete="off"
-          onChange={(e) => setCurrentMessage(e.target.value)}
-          onKeyDown={(e) => {
-            e.key === "Enter" && sendMessage();
-          }}
-        />
-        <button className="btn" onClick={sendMessage}>
-          <i className="fas fa-paper-plane"></i> Send
-        </button>
-        {/* </form> */}
       </div>
-    </div>
+    </section>
   );
 }
